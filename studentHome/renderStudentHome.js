2cd37d667b5eb2b70d10585b8fc444a9b47b6324
Aaron Zhang <a.zhang1997@outlook.com>
2021-03-09 21:22:32 -0500

    created welcome page and reorganized

--- studentHome/renderStudentHome.js
+++ studentHome/renderStudentHome.js
@@ -1,54 +1,49 @@
+let pageNum = 1;
+
 export async function renderNavbar() {
   return `
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-              <a class="navbar-item" href="../index.html">
-                  <img src="/media/learnscaping_logo.png" width="210">
-              </a>
-  
-              <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-              </a>
-          </div>
-  
-          <div id="navbarBasicExample" class="navbar-menu">
-              <div class="navbar-start">
-                  <a class="navbar-item" href="../index.html">
-                      Home
-                  </a>
-  
-                  <a class="navbar-item" href="courses.html">
-                    Courses
-                  </a>
-  
-                  <a class="navbar-item" href="../toolBoxPage/toolBox.html">
-                      Tool Box
-                  </a>
-          </div>
-  
-          <div class="navbar-end">
-              <div class="navbar-item">
-                  <div class="buttons">
-                      <a class="button is-success">
-                          <strong>Sign up</strong>
-                      </a>
-                      <a class="button is-light">
-                          Log in
-                      </a>
-                  </div>
-              </div>
-          </div>
-      </div>
-      </nav> `;
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="studentHome.html">
+                <img src="../media/learnscaping_logo.png" width="210">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="studentHome.html">
+                    Home
+                </a>
+
+                <a class="navbar-item" href="../lessonPage/lessonPage.html">
+                    Example Lesson Page
+                </a>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-success" href="">
+                        <strong>Sign Out</strong>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </nav> `;
 }
 
 export async function renderBody() {
   return `
       <section class="section">
       <div class="container">
-            <h1 class="title">Courses</h1>
+            <h1 class="title is-1">Courses</h1>
             ${await renderCourses()}
       </div>
       </section>
@@ -164,12 +159,57 @@ export async function renderPage() {
   return html;
 }
 
+function recalculateButtons() {
+  console.log("now on page " + pageNum);
+  if (pageNum == 1) {
+    $(".pagination-previous").attr("disabled", true);
+  } else if (pageNum == 3) {
+    $(".pagination-next").attr("disabled", true);
+  } else {
+    $(".pagination-previous").attr("disabled", false);
+    $(".pagination-next").attr("disabled", false);
+  }
+  switch (pageNum) {
+    case 1:
+      $(".content").empty();
+      $(".content").append(renderPage1());
+      break;
+    case 2:
+      $(".content").empty();
+      $(".content").append(renderPage2());
+      break;
+    case 3:
+      $(".content").empty();
+      $(".content").append(renderPage3());
+      break;
+  }
+}
+
 export async function loadIntoDOM() {
   const $root = $("#root");
 
   renderPage();
 
   $root.append(await renderPage());
+
+  $(".pagination-previous").on("click", () => {
+    if (pageNum <= 1) {
+      return;
+    }
+    // increment by 100/size of section deck
+    document.getElementById("sProgress").value -= 33;
+    pageNum--;
+    recalculateButtons();
+  });
+  $(".pagination-next").on("click", () => {
+    if (pageNum >= 3) {
+      return;
+    }
+    // increment by 100/size of section deck
+    document.getElementById("sProgress").value += 33;
+    pageNum++;
+    recalculateButtons();
+  });
 }
 
 $(function () {
