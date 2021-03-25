7ba90f1093d6bce4c9c51cbd42c1cc1256285e1d
Ben Rosenberger <brose32@live.unc.edu>
2021-03-25 18:05:40 -0400

    added firebase call for json files

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin')
-require('firebase/storage');
+require('@firebase/storage');
+const axios = require('axios')
 
 
 var serviceAccount = JSON.parse(process.env.GOOGLE_FIREBASE_AUTH);
@@ -13,7 +14,7 @@ var storage = admin.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.bucket('gs://learnscapingunc-58b67.appspot.com');
 
-
+//returns a temporary link to a file
 exports.getLinkToFile = function(filename) {
     const urlOptions = {
         action: "read",
@@ -27,7 +28,7 @@ exports.getLinkToFile = function(filename) {
     });
 }
 
-
+//uploads file to google firebase storage
 exports.uploadFile = function(filename) {
     return storageRef.upload(filename).then((response) => {
         console.log("file uploaded")
@@ -35,9 +36,21 @@ exports.uploadFile = function(filename) {
         console.log(err)
     })
 }
-
+//deletes file from google firebase storage
 exports.deleteFile = function(filename) {
     return storageRef.file(filename).delete().then((res) => {
         console.log(`${filename} deleted`)
     })
+}
+//returns a json object of the specified coursename with all of the course text content
+exports.getWebpageData = async function(coursename) {
+    try {
+        const link = await this.getLinkToFile(coursename + '.json');
+        var res = await axios.get(link);
+        console.log(res.data)
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
