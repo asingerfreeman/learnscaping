3c65c3692682dbaa485b4031fae4af9266b427dc
Aaron Zhang <a.zhang1997@outlook.com>
2021-03-29 21:15:03 -0400

    Update renderCreateTest.js
    redesigned, added button functionality

--- createTestPage/renderCreateTest.js
+++ createTestPage/renderCreateTest.js
@@ -2,7 +2,7 @@ export async function renderNavbar() {
     return `
       <nav class="navbar" role="navigation" aria-label="main navigation">
           <div class="navbar-brand">
-              <a class="navbar-item" href="studentHome.html">
+              <a class="navbar-item" href="../instructorHome.instructorHome.html">
                   <img src="../media/learnscaping_logo.png" width="210">
               </a>
   
@@ -19,46 +19,41 @@ export async function renderNavbar() {
                       Home
                   </a>
           </div>
-  
-          <div class="navbar-end">
-              <div class="navbar-item">
-                  <div class="buttons">
-                      <a class="button is-success" href="">
-                          <strong>Sign Out</strong>
-                      </a>
-                  </div>
-              </div>
-          </div>
       </div>
       </nav> `;
 }
 
-export async function renderBody() {
-    let id = ID();
-
+export async function renderTitleForm() {
     return `
-    <section class="section">
-        <div class="container">
-            <h1 class="title is-1">New Test</h1>
-            <form class="box">
-                ${await renderQuestionForm(id)}
-                <div id="nextQuestionArea">
-                </div>
-
-                <div class="buttons">
-                    <button class="button is-success">Add Question</button>
-                    <button class="button">Create Test</button>
-                </div>
-            </form>
+    <form id="replace" class="box">
+        <h1 class="title">Add Title</h1>
+        <div class="field">
+            <div class="control">
+                <input class="input" type="text" placeholder="Title">
+            </div>
         </div>
-    </section>
+
+        <button id="submitTitleButton" type="submit" class="button is-success">Submit Title</button>
+    </form>
     `;
 }
 
-export async function renderQuestionForm(id) {
+export async function renderAddQuestionForm() {
     return `
-    <div class="box" id="${id}">
-        <h1 class="title">Question</h1>
+    <form id="replace" class="box">
+        <h1 class="title">Add Question/Finish</h1>
+        <div class="buttons">
+            <button id="addQuestionButton" class="button is-success">Add Question</button>
+            <button id="finishButton" class="button">Finish</button>
+        </div>
+    </form>
+    `;
+}
+
+export async function renderQuestionForm() {
+    return `
+    <form id="replace" class="box">
+        <h1 class="title">Add Question</h1>
         <div class="field">
             <label class="label">Question</label>
             <div class="control">
@@ -112,9 +107,68 @@ export async function renderQuestionForm(id) {
         </div>
         
         <div class="buttons is-right">
-            <button class="button is-danger">Delete</button>
+            <button id="submitQuestionButton" type="submit" class="button is-success">Submit Question</button>
+            <button id="cancelButton" class="button">Cancel</button>
         </div>
-    </div>`;
+    </form>`;
+}
+
+export async function renderBody() {
+    return `
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-1">Create Test</h1>
+            ${await renderTitleForm()}
+        </div>
+    </section>
+    `;
+}
+
+export async function renderFinish() {
+    return `
+    <form id="replace" class="box">
+        <h1 class="title">Test Created!</h1>
+        <p>
+            Please view and edit your new test from the home page.
+        <p>
+    </form>
+    `;
+}
+
+export async function renderPage() {
+    let html = await renderNavbar();
+
+    html += await renderBody();
+
+    return html;
+}
+
+export async function handleSubmitTitleButtonPress() {
+    //*************** TODO: ADD CHECK FOR NO EMPTY TITLE ****************** */
+
+    //************ */ TODO: ADD API CALL TO CREATE NEW TEST OBJECT WITH TITLE PARAM ******************************
+
+    $("#replace").replaceWith(`${await renderQuestionForm()}`);
+}
+
+export async function handleSubmitQuestionButtonPress() {
+    // ************ TODO: ADD CHECK FOR NO EMPTY ENTIRES AND CHECK FOR ONE CORRECT ANSWER *************************
+
+    //************ */ TODO: ADD API CALL  ***************************************
+
+    $("#replace").replaceWith(`${await renderAddQuestionForm()}`);
+}
+
+export async function handleCancelButtonPress() {
+    $("#replace").replaceWith(`${await renderAddQuestionForm()}`);
+}
+
+export async function handleAddQuestionButtonPress() {
+    $("#replace").replaceWith(`${await renderQuestionForm()}`);
+}
+
+export async function handleFinishButtonPress() {
+    $("#replace").replaceWith(`${await renderFinish()}`);
 }
 
 //Generates a random question ID
@@ -125,20 +179,17 @@ var ID = function () {
     return "_" + Math.random().toString(36).substr(2, 9);
 };
 
-export async function renderPage() {
-    let html = await renderNavbar();
-
-    html += await renderBody();
-
-    return html;
-}
-
 export async function loadIntoDOM() {
     const $root = $("#root");
 
     $root.append(await renderPage());
 
     //**************** TODO: Add button functionality ************************************************/
+    $root.on("click", "#submitTitleButton", handleSubmitTitleButtonPress);
+    $root.on("click", "#submitQuestionButton", handleSubmitQuestionButtonPress);
+    $root.on("click", "#cancelButton", handleCancelButtonPress);
+    $root.on("click", "#addQuestionButton", handleAddQuestionButtonPress);
+    $root.on("click", "#finishButton", handleFinishButtonPress);
 }
 
 $(function () {
