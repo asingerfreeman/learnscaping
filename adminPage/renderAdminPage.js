89d3e2c608b3a8cd9cf6742fb5f3ade4ad53a068
Garrett <garrett9@live.unc.edu>
2021-04-09 13:44:44 -0400

    finished admin view

--- adminPage/renderAdminPage.js
+++ adminPage/renderAdminPage.js
@@ -41,6 +41,7 @@ export async function renderBody() {
               ${await renderCourses()}
         </div>
         </section>
+        
         `;
 }
 
@@ -48,7 +49,7 @@ export async function renderCourses() {
   return `
     <div class="box">
   <h2 class="title">Accounts</h2>
-  <table class="table">
+  <table class="table is-striped">
   <thead>
     <tr>
       <th>Name</th>
@@ -58,6 +59,16 @@ export async function renderCourses() {
   <tbody>
   </tbody>
 </table>
+<article id="message" class="message is-info">
+        <div class="message-header">
+            <p>Info</p>
+        </div>
+        <div class="message-body">
+            <strong>You can use this page to toggle course completeness and instructor status</strong><br><br>
+            - "Completed" checkboxes are disabled unless the course has been assigned to the account<br> 
+            - All accounts registered in the system appear here<br>
+        </div>
+    </article>
 </div>`
 }
 
@@ -103,47 +114,51 @@ async function handleCompleteToggleClick(event) {
   let courseID = event.target.name;
   let course;
   const query = await ref.get().then((doc) => {
-      let courses = doc.data().courses
-      course = courses.find(c => c.cid === courseID)
+    let courses = doc.data().courses
+    course = courses.find(c => c.cid === courseID)
   }).catch((error) => {
-      console.log("Error getting document:", error)
+    console.log("Error getting document:", error)
   })
   if (!event.target.checked) {
-      // then not completed
-      let courseObj = {
-        cid: course.cid,
-        isStarted: course.isStarted,
-        isComplete: false,
-        testScore: course.testScore
-      }
-      return ref.update({
-          // courses: firebase.firestore.FieldValue.arrayUnion(courseObj)
-      })
-          .then(() => {
-              console.log("Document successfully updated!");
-          })
-          .catch((error) => {
-              // The document probably doesn't exist.
-              console.error("Error updating document: ", error);
-          });
+    // then not completed
+    let newCourseObj = {
+      cid: course.cid,
+      isStarted: course.isStarted,
+      isComplete: false,
+      testScore: course.testScore
+    }
+    ref.update({
+      courses: firebase.firestore.FieldValue.arrayRemove(course),
+    })
+    return ref.update({
+      courses: firebase.firestore.FieldValue.arrayUnion(newCourseObj)
+    }).then(() => {
+      console.log("Document successfully updated!");
+    })
+      .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
   } else {
-      // then completed
-      let courseObj = {
-        cid: course.cid,
-        isStarted: course.isStarted,
-        isComplete: true,
-        testScore: course.testScore
-      }
-      return ref.update({
-          // courses: firebase.firestore.FieldValue.arrayUnion(courseObj)
-      })
-          .then(() => {
-              console.log("Document successfully updated!");
-          })
-          .catch((error) => {
-              // The document probably doesn't exist.
-              console.error("Error updating document: ", error);
-          });
+    // then completed
+    let newCourseObj = {
+      cid: course.cid,
+      isStarted: course.isStarted,
+      isComplete: true,
+      testScore: course.testScore
+    }
+    ref.update({
+      courses: firebase.firestore.FieldValue.arrayRemove(course),
+    })
+    return ref.update({
+      courses: firebase.firestore.FieldValue.arrayUnion(newCourseObj)
+    }).then(() => {
+      console.log("Document successfully updated!");
+    })
+      .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
   }
 }
 
@@ -196,7 +211,7 @@ export async function loadIntoDOM() {
     for (let j = 0; j < courses.length; j++) {
       $(`table tbody tr[name="${i}"]`).append(`
               <td>
-                  <input type="checkbox" name="${courseIDs[j]}" class="complete" id="${userIDs[i]}">
+                  <input type="checkbox" name="${courseIDs[j]}" class="complete" id="${userIDs[i]}" disabled>
                   Completed
                   <div class="statusAppend" name="${courseIDs[j]}" id="${userIDs[i]}"></div>
               </td>`)
@@ -204,13 +219,24 @@ export async function loadIntoDOM() {
     }
   }
 
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < users[i].courses.length; j++) {
+      $(`input[name=${users[i].courses[j].cid}][id=${userIDs[i]}]`).attr('checked', false)
+      $(`input[name=${users[i].courses[j].cid}][id=${userIDs[i]}]`).attr('disabled', false)
+      if (users[i].courses[j].isComplete) {
+        $(`input[name=${users[i].courses[j].cid}][id=${userIDs[i]}]`).attr('checked', true)
+        $(`input[name=${users[i].courses[j].cid}][id=${userIDs[i]}]`).attr('disabled', false)
+      } 
+    }
+}
+
   $('.isInstr').on("change", () => {
     handleInstructorToggleClick()
   });
 
   $('.complete').on("change", () => {
-    // handleCompleteToggleClick(event)
-});
+    handleCompleteToggleClick(event)
+  });
 }
 
 $(function () {
