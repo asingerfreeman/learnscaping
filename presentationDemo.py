3e31d5ed47a5bb12e045975004a6b46bf67e7a59
brose32 <46757280+brose32@users.noreply.github.com>
2021-04-10 18:40:06 -0400

    updated demo

--- presentationDemo.py
+++ presentationDemo.py
@@ -1,25 +1,35 @@
-#from selenium import webdriver
-#import time
-#PATH = "C:\Program Files (x86)\chromedriver.exe"
-#realoptions = webdriver.ChromeOptions()
-#realoptions.add_experimental_option('excludeSwitches', ['enable-logging'])
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+import time
 
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+realoptions = webdriver.ChromeOptions()
+#realoptions.add_argument("--headless")
+#realoptions.add_argument("--disable-gpu")
+#realoptions.add_argument("--no-sandbox")
+realoptions.add_experimental_option('excludeSwitches', ['enable-logging'])
 
+driver = webdriver.Chrome(PATH, options=realoptions)
+driver.maximize_window()
+driver.get("https://cs.unc.edu")
 
-#driver = webdriver.Chrome(PATH, options=realoptions)
+#search_input = WebDriverWait(driver, 10).until(
+#    expected_conditions.presence_of_element_located((By.NAME, "s"))
+#)
+#search_input.send_keys("stotts")
+#submit = driver.find_element_by_class_name('search-submit')
+#submit.click()
 
-#driver.get("https://cs.unc.edu")
+#results = WebDriverWait(driver, 10).until(
+#    expected_conditions.presence_of_all_elements_located((By.CLASS_NAME, "gsc-result"))
+#)
 
-#element = driver.find_element_by_name('s')
-#element.send_keys("stotts")
+#titles = driver.find_elements_by_css_selector('a.gs-title')
 
-#element = driver.find_element_by_class_name('search-submit')
-#element.click()
-
-#elements = driver.find_elements_by_class_name('gs-title')
-
-#for element in elements:
-#    print(element.text)
+#for title in titles:
+#    print(title.text)
 
 
 #EXAMPLE 2
@@ -37,4 +47,4 @@
 #for element in elements:
 #    print(element.text)
 #    print("")
-#driver.quit()
+driver.quit()
